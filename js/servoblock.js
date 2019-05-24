f30749c0db333b8b65a2423b036a86cd534bbb17
Błażej Sowa <bsowa123@gmail.com>
2019-05-24 17:47:38 +0200

    some style improvements

--- js/servoblock.js
+++ js/servoblock.js
@@ -2,9 +2,9 @@ function servoHide() {
     var x = document.getElementById('servoB');
     if (x.style.display === "none") {
       x.style.display = 'block';
-      document.getElementById('servoBtn').innerHTML = 'Servo disable';
+      document.getElementById('servoBtn').innerHTML = 'Hide servos';
     } else {
       x.style.display = 'none';
-      document.getElementById('servoBtn').innerHTML = 'Servo enable';
+      document.getElementById('servoBtn').innerHTML = 'Show servos';
     }
   } 
