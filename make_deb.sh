9cd1983cdd8a66af2b539a420a03ecf295b0da55
Błażej Sowa <bsowa123@gmail.com>
2020-06-10 22:12:38 +0200

    add make_deb script

---
+++ make_deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -f *.deb
+
+fpm --input-type dir \
+	--output-type deb \
+	--maintainer "Fictionlab <support@fictionlab.pl>" \
+	--name "leo-ui" \
+	--vendor "Kell ideas Ltd." \
+	--license "MIT" \
+	--url "https://github.com/LeoRover/leo_ui" \
+	--version "$(git describe --tags)" \
+	--architecture all \
+	--deb-no-default-config-files \
+    --exclude .git \
+    --exclude make_deb.sh \
+	--deb-dist bionic \
+	--verbose \
+	--prefix "/opt/leo_ui" \
+	--description "Leo Rover Web User Interface" \
+    .
