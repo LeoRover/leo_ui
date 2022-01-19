e800b26f8a6d8eb205798f469b4edbd2b4fe0d5a
Bitterisland6 <2010olo@wp.pl>
2022-01-19 19:43:50 +0100

    trying to switch video src with select
    Signed-off-by: Bitterisland6 <2010olo@wp.pl>

--- js/cameraList.js
+++ js/cameraList.js
@@ -1,4 +1,7 @@
 var select;
+var video;
+var robot_hostname;
+
 
 function createOptions() {
     if(select.childElementCount == 0) {
@@ -10,8 +13,15 @@ function createOptions() {
     }
 }
 
+function changeVideoSrc() {
+    var selected = select.selectedIndex;
+    video.src = "http://" + robot_hostname + ":8080/stream?topic=" + cameraTopics[selected] + "camera/image_raw&type=ros_compressed";
+}
+
 function test() {
+    robot_hostname = location.hostname;
     select = document.getElementById('camera-select');
+    video = document.getElementById('video');
     console.log(select);
     console.log(select.childElementCount);
     const timeout = setTimeout(createOptions, 3000);
